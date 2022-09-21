eb0a33fb59e4057c6f2ed1d5d1a0cec417fa04fe
Laura Fabbiano <lfabbian@c1r15s7.42lausanne.ch>
2022-09-21 13:26:51 +0200

    Corrections

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -6,37 +6,50 @@
 /*   By: lfabbian <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 19:37:04 by lfabbian          #+#    #+#             */
-/*   Updated: 2022/09/19 18:17:22 by lfabbian         ###   ########.fr       */
+/*   Updated: 2022/09/21 09:04:19 by lfabbian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_putchar(char value)
+{
+	write(1, &value, 1);
+}
+
+void	ft_result(char u, char d, char c)
+{
+	ft_putchar(c);
+	ft_putchar(d);
+	ft_putchar(u);
+	if (! (c == '7' && d == '8' && u == '9'))
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
 void	ft_print_comb(void)
 {
-	int	c;
-	int	d;
-	int	u;
+	char	u;
+	char	d;
+	char	c;
 
 	c = '0';
-	d = '1';
-	u = '2';
 	while (c <= '7')
 	{
+		d = c + 1;
 		while (d <= '8')
 		{
+			u = d + 1;
 			while (u <= '9')
 			{
-				write(1, &c, 1);
-				write(1, &d, 1);
-				write(1, &u, 1);
-				if (c != '7')
-					write(1, ",", 1);
+				ft_result(u, d, c);
 				u++;
 			}
-			u = ++d + 1;
+			d++;
 		}
-		d = ++c;
+		c++;
 	}
 }
 /*#include <stdio.h>
--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -6,53 +6,67 @@
 /*   By: lfabbian <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 12:40:21 by lfabbian          #+#    #+#             */
-/*   Updated: 2022/09/20 10:35:27 by lfabbian         ###   ########.fr       */
+/*   Updated: 2022/09/21 09:05:35 by lfabbian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_numbers(int a, int b, int c, int d)
+void	ft_putchar(char value)
 {
-	write(1, &a, 1);
-	write(1, &b, 1);
-	write(1, " ", 2);
-	write(1, &c, 1);
-	write(1, &d, 1);
-	if (a != '9' || b != '8' || c != '9' || d != '9')
-		write (1, ", ", 2);
+	write(1, &value, 1);
 }
 
-void	fr_print_comb2(void)
+void	ft_write(int value)
 {
-	int	tab[6];
+	int	x;
+	int	y;
 
-	tab[0] = 1;
-	tab[1] = 0;
-	tab[2] = 0;
-	tab[3] = 0;
-	tab[4] = 0;
-	tab[5] = 0;
-	if ((tab[0] <= 99 && tab[1] < 99))
+	if (value > 9)
 	{
-		while (tab[1] < 99)
-		{
-			while (tab[0] <= 99)
-			{
-				tab[2] = tab[0] / 10;
-				tab[3] = tab[0] % 10;
-				ft_numbers(tab[4] + 48, tab[5] + 48, tab[2] + 48, tab[3] + 48);
-				tab[0]++;
-			}
-			tab[1]++;
-			tab[4] = tab[1] / 10;
-			tab[5] = tab[1] % 10;
-			tab[0] = tab[1] + 1;
-		}
+		x = value / 10;
+		y = value % 10;
+		ft_putchar(x + '0');
+		ft_putchar(y + '0');
+	}
+	else
+	{
+		ft_putchar('0');
+		ft_putchar(value + 48);
 	}
 }
 
-/*int    main(void)
+void	ft_result(int x, int y)
 {
-    fr_print_comb2();
+	ft_write(x);
+	ft_putchar(' ');
+	ft_write(y);
+	if (x < 98 || y < 99)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int	x;
+	int	y;
+
+	x = 0 ;
+	while (x <= 99)
+	{
+		y = x + 1;
+		while (y <= 99)
+		{
+			ft_result(x, y);
+			y++;
+		}
+		x++;
+	}
+}
+
+/*int main()
+{
+	ft_print_comb2();
 }*/
