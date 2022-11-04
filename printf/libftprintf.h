b2220a8d7e6cab7203d1beaa2fd11cde2943c382
Laura Fabbiano <lfabbian@c1r7s5.42lausanne.ch>
2022-11-04 10:04:53 +0100

    start of ft_printf

---
+++ printf/libftprintf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfabbian <lfabbian@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/04 09:52:42 by lfabbian          #+#    #+#             */
+/*   Updated: 2022/11/04 10:02:12 by lfabbian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+
+int ft_printf(const char *, ...);
+
+#endif
