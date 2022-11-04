d66563a26a1096cc5c4ac2d352eb0a281173406f
Laura Fabbiano <lfabbian@c1r7s7.42lausanne.ch>
2022-11-04 17:51:58 +0100

    added little things

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,17 +6,18 @@
 /*   By: lfabbian <lfabbian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 09:51:03 by lfabbian          #+#    #+#             */
-/*   Updated: 2022/11/04 13:43:18 by lfabbian         ###   ########.fr       */
+/*   Updated: 2022/11/04 17:51:25 by lfabbian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int ft_printf(const char *, ...)
+int ft_printf(const char *print_details, ...)
 {
+    int     i;
     va_list args;
 
-    va_start (args, format);
+    va_start (args, print_details);
     va_end (args);
 }
 
