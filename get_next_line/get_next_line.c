92f487903da1bab4f20f60b0391cb559fc51eca2
Laura Fabbiano <lfabbian@c1r7s5.42lausanne.ch>
2022-11-30 14:55:11 +0100

    serious start

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,29 +6,61 @@
 /*   By: lfabbian <lfabbian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 16:43:10 by lfabbian          #+#    #+#             */
-/*   Updated: 2022/11/29 14:28:55 by lfabbian         ###   ########.fr       */
+/*   Updated: 2022/11/30 14:54:21 by lfabbian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line()
+char *get_next_line(int fd)
 {
-	int fd;
-	int	bytes_read;
-	char	*buffer;
-
+	static char	*stash;
+	char 		*buffer;
+	char 		*line;
+	
 	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	fd = open("text.txt", O_RDONLY);
-	if (fd == -1)
+	if (!buffer)
 		return (NULL);
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	buffer[bytes_read] = 0;
+	stash = stash_filling(fd, stash, buffer);
+	line = extract_line(stash);
+	stash = extract_new_stash(stash);
+	return(line);
+}
 
-	return (buffer);
+char	*stash_filling(int fd, char *stash, char *buffer)
+{
+	int	i;
+	ssize_t	nbytes;
+
+	i = 0;
+	nbytes = read(fd, buffer, BUFFER_SIZE);
+	buffer[nbytes] = 0;
+	stash = ft_strjoin(stash, buffer);
+	while (stash[i])
+	{
+		if (stash[i] == ft_is_bsn(stash[i]))
+			return (stash);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*extract_new_stash()
+{
+	return (NULL);
+}
+
+char	*extract_line()
+{
+	return(NULL);
 }
 
 int main()
 {
-	printf("%s\n", get_next_line());
+	int	fd;
+
+	fd = open("text.txt", O_RDONLY);
+	printf("%s\n", get_next_line(fd));
+	printf("%s\n", get_next_line(fd));
+	printf("%s\n", get_next_line(fd));
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: lfabbian <lfabbian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 11:08:48 by lfabbian          #+#    #+#             */
-/*   Updated: 2022/11/29 14:25:13 by lfabbian         ###   ########.fr       */
+/*   Updated: 2022/11/30 13:47:13 by lfabbian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#   define BUFFER_SIZE 2
+#   define BUFFER_SIZE 10
 # endif
 # include <stdlib.h>
 # include <unistd.h>
@@ -21,5 +21,14 @@
 # include <stdio.h>
 
 char	*get_next_line();
+char	*stash_filling(int fd, char *stash, char *buffer);
+char	*extract_line();
+char	*extract_new_stash();
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s1);
+int     ft_is_bsn(int c);
+size_t	ft_strlen(const char *s);
 
 #endif
