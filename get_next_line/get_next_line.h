fc737b94cbbc73016a343f5ab62c0fd752371d1e
Brisly <84352348+thebrisly@users.noreply.github.com>
2022-12-06 17:04:23 +0100

    functions updated

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -13,22 +13,21 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#   define BUFFER_SIZE 50
+#  define BUFFER_SIZE 20
 # endif
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdio.h>
 
-char	*get_next_line();
+char	*get_next_line(int fd);
 char	*stash_filling(int fd, char *stash, char *buffer);
-char	*extract_line();
-char	*extract_new_stash();
+char	*extract_line(char *stash, char *line);
+char	*extract_new_stash(char *stash);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strdup(const char *s1);
-int     ft_is_bsn(int c);
 size_t	ft_strlen(const char *s);
 
 #endif
