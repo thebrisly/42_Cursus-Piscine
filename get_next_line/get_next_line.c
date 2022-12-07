a9dcc224f7644ea90c78c41b258a9916260ae82b
Brisly <84352348+thebrisly@users.noreply.github.com>
2022-12-07 11:35:24 +0100

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -9,7 +9,6 @@
 /*   Updated: 2022/12/04 16:55:09 by lfabbian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
@@ -20,6 +19,14 @@ char	*get_next_line(int fd)
 
 	line = NULL;
 	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+    {
+        free(stash);
+        free(buffer);
+        stash = NULL;
+        buffer = NULL;
+        return (NULL);
+    }
 	if (!buffer)
 		return (NULL);
 	stash = stash_filling(fd, stash, buffer);
@@ -32,15 +39,22 @@ char	*stash_filling(int fd, char *stash, char *buffer)
 {
 	ssize_t	nbytes;
 
+	nbytes = 1;
 	if (!stash)
 			stash = ft_strdup(buffer);
-	while (!ft_strchr(stash, '\n') && nbytes != 0)
-	{
+	while (!ft_strchr(stash, '\n') && nbytes > 0)
+	{ 
 		nbytes = read(fd, buffer, BUFFER_SIZE);
+		if (nbytes == -1)
+		{
+			free (buffer);
+			free (stash);
+			return (NULL);
+		}
 		buffer[nbytes] = 0;
 		stash = ft_strjoin(stash, buffer);
 	}
-	//free (buffer);
+	free (buffer);
 	return (stash);
 }
 
@@ -70,7 +84,7 @@ char	*extract_line(char *stash, char *line)
 	i = 0;
 	while (stash[len] != '\n' && stash[len])
 		len++;
-	line = malloc((len + 1) * sizeof(char));
+	line = malloc((len + 2) * sizeof(char));
 	if (!line)
 		return (NULL);
 	while (i < len)
@@ -78,7 +92,8 @@ char	*extract_line(char *stash, char *line)
 		line[i] = stash[i];
 		i++;
 	}
-	line[i] = 0;
+	line[i] = '\n';
+	line[i + 1] = 0;
 	return (line);
 }
 
@@ -87,13 +102,13 @@ int main()
 	int	fd;
 
 	fd = open("text.txt", O_RDONLY);
-	printf("GNL 1: %s\n", get_next_line(fd));
-	printf("GNL 2: %s\n", get_next_line(fd));
-	printf("GNL 3: %s\n", get_next_line(fd));
-	printf("GNL 4: %s\n", get_next_line(fd));
-	printf("GNL 5: %s\n", get_next_line(fd));
-	printf("GNL 6: %s\n", get_next_line(fd));
-	printf("GNL 7: %s\n", get_next_line(fd));
-	printf("GNL 8: %s\n", get_next_line(fd));
-	printf("GNL 9: %s\n", get_next_line(fd));
+	printf("GNL 1: %s", get_next_line(fd));
+	printf("GNL 2: %s", get_next_line(fd));
+	printf("GNL 3: %s", get_next_line(fd));
+	printf("GNL 4: %s", get_next_line(fd));
+	printf("GNL 5: %s", get_next_line(fd));
+	printf("GNL 6: %s", get_next_line(fd));
+	printf("GNL 7: %s", get_next_line(fd));
+	printf("GNL 8: %s", get_next_line(fd));
+	printf("GNL 9: %s", get_next_line(fd));
 }
