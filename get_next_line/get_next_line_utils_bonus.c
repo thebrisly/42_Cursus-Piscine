62aaf7bbc736142a2970faf822dc74a51028f391
Brisly <84352348+thebrisly@users.noreply.github.com>
2022-12-14 09:53:59 +0100

    final GNL with bonuses
    nothing changed in this file compared to the classic GNL

--- get_next_line/get_next_line_utils_bonus.c
+++ get_next_line/get_next_line_utils_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 size_t	ft_strlen(const char *s)
 {
