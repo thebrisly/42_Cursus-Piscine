31c060956769b8abd3bfc0367b53a5317b71cb4d
Laura Fabbiano <lfabbian@c1r7s8.42lausanne.ch>
2022-12-23 15:00:46 +0100

    minitalk

---
+++ minitalk/minitalk.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfabbian <lfabbian@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/23 13:55:42 by lfabbian          #+#    #+#             */
+/*   Updated: 2022/12/23 13:56:41 by lfabbian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <signal.h>
+
+#endif
