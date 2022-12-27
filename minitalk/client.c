d73345f9f516032083d9daad28271b95bd547dee
Lau <laura.fabbiano.gomez@hotmail.com>
2022-12-27 15:22:15 +0100

    minitlak modifs

--- minitalk/client.c
+++ minitalk/client.c
@@ -3,12 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lfabbian <lfabbian@student.42.fr>          +#+  +:+       +#+        */
+/*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 13:53:56 by lfabbian          #+#    #+#             */
-/*   Updated: 2022/12/23 13:56:57 by lfabbian         ###   ########.fr       */
+/*   Updated: 2022/12/26 09:51:19 by brisly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void send_message(int pid, char *message)
+{
+	int	i;
+
+	i = 0;
+	kill(pid, SIGUSR1); // Envoi du signal SIGUSR1 au serveur pour signaler qu'un message va être envoyé
+	while (message[i++])
+	{
+		kill(pid, message[i]); // Envoi du caractère c au serveur
+		usleep(100); // Pause pour laisser le temps au serveur de traiter le caractère reçu
+	}
+	kill(pid, 0); // Envoi d'un caractere null pour signaler la fin du message
+}
+
+int	main(int argc, char **argv)
+{
+	char	*message;
+	int		*server_id;
+
+	server_id = atoi(argv[1]);
+	message = argv[2];
+	if (argc == 3)
+		send_message(server_id, message);
+}
--- minitalk/server.c
+++ minitalk/server.c
@@ -3,19 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lfabbian <lfabbian@student.42.fr>          +#+  +:+       +#+        */
+/*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 13:53:54 by lfabbian          #+#    #+#             */
-/*   Updated: 2022/12/23 15:00:17 by lfabbian         ###   ########.fr       */
+/*   Updated: 2022/12/26 09:51:09 by brisly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+char message[1024]; // Tableau de caractères pour stocker le message reçu
+unsigned int messageIndex = 0; // Indice du prochain caractère à recevoir dans le message
+
+// Fonction de gestion des signaux
+void signal_handler(int signum)
+{
+	// Si le signal reçu est SIGUSR1, cela signifie qu'un message est en cours de transmission
+	if (signum == SIGUSR1)
+	{
+		messageIndex = 0;
+	}
+	// Si le signal reçu est un entier non signé (unsigned int), cela signifie qu'il s'agit d'un caractère du message
+	else if (signum >= 0)
+	{
+		message[messageIndex] = (char)signum;
+		messageIndex++;
+	}
+}
+
+	// Structure de gestion des signaux
+	struct sigaction action;
+	action.sa_handler = signal_handler;
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = 0;
+
+	// Enregistrement de la fonction signal_handler pour les signaux SIGUSR1 et tous les entiers non signés (unsigned int)
+	sigaction(SIGUSR1, &action, NULL);
+	for (unsigned int i = 0; i < 32; i++)
+		{
+		sigaction(i, &action, NULL);
+		}
+
+	// Boucle infinie pour attendre les messages des clients
+	while (1)
+	{
+		// Si le message est complet, on l'affiche et on remet l'indice du prochain caractère à 0
+		if (message[messageIndex] == '\0')
+		{
+			printf("Message reçu : %s\n", message);
+			messageIndex = 0;
+		}
+		usleep(100);
+	}
+	return 0;
+}
+
 int	main()
 {
+	static char	message;
 	int	pid;
 
 	pid = getpid();
 	printf("Server's PID: %d\n", pid); //PID always changes
+	while (1) // Boucle infinie pour attendre le message des clients
+		usleep();
 }
