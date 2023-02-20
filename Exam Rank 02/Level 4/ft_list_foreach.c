8418f4a4b1fb258990da0b6b911da217b2621c67
Lau <laura.fabbiano.gomez@hotmail.com>
2023-02-20 22:15:35 +0100

    added new exercises for exam

---
+++ Exam Rank 02/Level 4/ft_list_foreach.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 22:00:00 by brisly            #+#    #+#             */
+/*   Updated: 2023/02/20 22:06:50 by brisly           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+typedef struct    s_list
+{
+    struct s_list *next;
+    void          *data;
+}                 t_list;
+
+void    ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	t_list *lst;
+
+	lst = begin_list;
+	while (lst)	
+	{
+		(*f)lst->data;
+		lst = lst->next;
+	}
+}
---
+++ Exam Rank 02/Level 4/ft_list_remove_if.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_remove_if.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 22:07:20 by brisly            #+#    #+#             */
+/*   Updated: 2023/02/20 22:11:16 by brisly           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+typedef struct      s_list
+{
+    struct s_list   *next;
+    void            *data;
+}                   t_list;
+
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+	if (begin_list == NULL || *begin_list == NULL)
+		return;
+
+	t_list *cur = *begin_list;
+
+	if (cmp(cur->data, data_ref) == 0)
+	{
+		*begin_list = cur->next;
+		free(cur);
+		ft_list_remove_if(begin_list, data_ref, cmp);
+	}
+	cur = *begin_list;
+	ft_list_remove_if(&cur->next, data_ref, cmp);
+}
