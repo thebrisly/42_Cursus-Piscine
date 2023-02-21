58ea342ed9976cf00fe44575741483ee1b7082db
Lau <laura.fabbiano.gomez@hotmail.com>
2023-02-21 08:58:24 +0100

    better pgcd.c

--- Exam Rank 02/Level 3/pgdc.c
+++ Exam Rank 02/Level 3/pgdc.c
@@ -6,32 +6,32 @@
 /*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 13:51:33 by brisly            #+#    #+#             */
-/*   Updated: 2023/02/20 21:57:55 by brisly           ###   ########.fr       */
+/*   Updated: 2023/02/21 08:45:08 by brisly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
-int		main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	if (argc == 3)
-	{
-		int number1 = atoi(argv[1]);
-		int number2 = atoi(argv[2]);
 
-		if (number1 >= 0 && number2 >= 0)
-		{
-			while (number1 != number2)
-			{
-				if (number1 > number2)
-					number1 = number1 - number2;
-				else
-					number2 = number2 - number1;
-			}
-			printf("%d", number1);
-		}
-	}
-	printf("\n");
+    if (argc == 3)
+    {
+        int number1 = atoi(argv[1]);
+        int number2 = atoi(argv[2]);
+        
+        if (number1 > 0 && number2 > 0)
+        {
+            while (number1 != number2)
+            {
+                if (number1 > number2)
+                    number1 = number1 - number2;
+                else
+                    number2 = number2 - number1;
+            }
+            printf("%d", number1);
+        }
+    }
+    printf("\n");
 }
