fd8dd681d2c7f5a336105bfdf51651941fc6306d
Lau <laura.fabbiano.gomez@hotmail.com>
2023-02-21 08:42:56 +0100

    improved print_hex

--- Exam Rank 02/Level 3/print_hex.c
+++ Exam Rank 02/Level 3/print_hex.c
@@ -6,38 +6,39 @@
 /*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 13:37:21 by brisly            #+#    #+#             */
-/*   Updated: 2023/02/20 21:35:08 by brisly           ###   ########.fr       */
+/*   Updated: 2023/02/21 08:40:58 by brisly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
 int ft_atoi(char *str)
 {
-	int result = 0;
-	int i = 0;
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		result = result * 10 + str[i] - '0';
-		i++;
-	}
-	return (result);
+    int atoi = 0;
+    int i = 0;
+    
+    while (str[i] && str[i] >= '0' && str[i] <= '9')
+    {
+        atoi = atoi * 10 + str[i] - '0';
+        i++;
+    }
+    return (atoi);
 }
 
-void	print_hex(int n)
+void print_hex(int number)
 {
-	char digits[] = "0123456789abcdef";
-	char base_16;
-
-	if (n >= 16)
-		print_hex(n / 16);
-	base_16 = digits[n % 16];
-	write(1, &base_16, 1);
+    char *digits = "0123456789abcdef";
+    
+    if (number >= 16)
+        print_hex(number / 16);
+    number = digits[number % 16];
+    write (1, &number, 1);
 }
 
 int main(int argc, char **argv)
 {
-	if (argc == 2)
-		print_hex(ft_atoi(argv[1]));
-	write (1, "\n", 1);
+    if (argc == 2)
+    {
+        print_hex(ft_atoi(argv[1]));
+    }
+    write(1,"\n",1);
 }
