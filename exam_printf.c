decd9341d349191296e548bc9bb9c41be003410b
Brisly <84352348+thebrisly@users.noreply.github.com>
2023-02-27 22:25:18 +0100

    exam 2nd solution

--- exam_printf.c
+++ exam_printf.c
@@ -103,3 +103,55 @@ int main ()
 	ft_printf("hello my name is %s and i'm %d", "laura", -23);
 	ft_printf("%d", ft_printf("hello my name is %s and i'm %d", "laura", -23));
 }
+
+// OR SECOND RIGHT RESPONSE (shorter)
+
+/*
+void	put_string(char *string, int *length)
+{
+	if (!string)
+		string = "(null)";
+	while (*string)
+		*length += write(1, string++, 1);
+}
+
+void	put_digit(long long int number, int base, int *length)
+{
+	char	*hexadecimal = "0123456789abcdef";
+
+	if (number < 0)
+	{
+		number *= -1;
+		*length += write(1, "-", 1);
+	}
+	if (number >= base)
+		put_digit((number / base), base, length);
+	*length += write(1, &hexadecimal[number % base], 1);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	int length = 0;
+	
+	va_list	pointer;
+	va_start(pointer, format);
+	
+	while (*format)
+	{
+		if ((*format == '%') && *(format + 1))
+		{
+			format++;
+			if (*format == 's')
+				put_string(va_arg(pointer, char *), &length);
+			else if (*format == 'd')
+				put_digit((long long int)va_arg(pointer, int), 10, &length);
+			else if (*format == 'x')
+				put_digit((long long int)va_arg(pointer, unsigned int), 16, &length);
+		}
+		else
+			length += write(1, format, 1);
+		format++;
+	}
+	return (va_end(pointer), length);
+}
+*/
