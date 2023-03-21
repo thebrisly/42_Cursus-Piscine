09af06ebd621f9e609929c127892f2328516f573
Brisly <84352348+thebrisly@users.noreply.github.com>
2023-03-21 14:15:44 +0100

    update cd.c

--- minishell/srcs/cd.c
+++ minishell/srcs/cd.c
@@ -12,17 +12,17 @@
 
 #include "../incs/minishell.h"
 
-
-
 void	set_dir(t_minishell *ms, char *dir)
 {
-	// char *tmp;
+	(void) dir;
+	char	*cwd;
 
-	// tmp = ft_strdup(dir);
-	replace_value(ms, "PWD")->value = ft_strdup(dir);
+	cwd = malloc(PATH_MAX);
+	getcwd(cwd, PATH_MAX);
+	replace_value(ms, "PWD")->value = cwd;
 	replace_value(ms, "OLDPWD")->value = ms->oldpwd;
-	ms->oldpwd = dir;
-	// free (tmp);
+	ms->oldpwd = cwd;
+	free (cwd);
 }
 
 void	mini_cd(t_minishell *ms)
@@ -34,11 +34,14 @@ void	mini_cd(t_minishell *ms)
 	int		new_dir;
 
 	argument = ms->args_tmp[1];
-	if (!argument)
+	if (!argument || (argument[0] == '~' && !argument[1]))
 	{
 		directory = get_value(ms, "HOME");
 		if (!directory)
-			printf("cd: HOME not set\n");
+		{
+			printf("cd: HOME not set\n"); //code d'erreur sur bash: 1
+			return ;
+		}
 	}
 	else
 	{
@@ -49,13 +52,8 @@ void	mini_cd(t_minishell *ms)
 		{
 			tmp = ft_substr(argument, 1, ft_strlen(ms->args_tmp[1]));
 			directory = ft_strjoin(get_value(ms, "HOME"), tmp);
-			if (!directory)
-				printf("cd: No such file or directory\n");
-			free (directory);
 			free (tmp);
 		}
-		else if (ft_strcmp("..", ms->args_tmp[1]) == 0)
-			directory = "..";
 		else
 		{
 			cwd = malloc(PATH_MAX);
@@ -64,13 +62,13 @@ void	mini_cd(t_minishell *ms)
 			directory = ft_strjoin(cwd, tmp);
 			free(cwd);
 			free (tmp);
-			free (directory);
+			// free (directory); ? pk ca marche meme quand je free
 		}
 	}
 	new_dir = chdir(directory);
 	if (new_dir == -1)
 	{
-		printf("cd: No such file or directory\n"); //code d'erreur sur bash: 1
+		printf("cd: %s: No such file or directory\n", argument); //code d'erreur sur bash: 1
 		return ;
 	}
 	set_dir(ms, directory);
