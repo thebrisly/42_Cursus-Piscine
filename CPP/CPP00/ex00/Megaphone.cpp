048ba6aaa87ed134ff3dbe5a658bf4192d76289d
thebrisly <laura.fabbiano.gomez@hotmail.com>
2023-05-31 13:48:29 +0200

    ex00 done

--- CPP/CPP00/ex00/Megaphone.cpp
+++ CPP/CPP00/ex00/Megaphone.cpp
@@ -6,13 +6,31 @@
 /*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 10:40:34 by brisly            #+#    #+#             */
-/*   Updated: 2023/05/31 11:44:10 by brisly           ###   ########.fr       */
+/*   Updated: 2023/05/31 13:48:14 by brisly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main()
+int main(int argc, char **argv)
 {
-    std::cout << "Hello World!\n";
+    (void) argc;
+    int words = 1;
+
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    else
+    {
+        while (words < argc)
+        {
+            int i = 0;
+            while (argv[words][i])
+            {
+                std::cout << (char) std::toupper(argv[words][i]);
+                i++;
+            }
+            words++;
+        }
+    }
+    std::cout << std::endl;
 }
