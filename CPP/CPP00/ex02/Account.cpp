01820244e49ae11d234d4bd3752a61e50fd8986f
thebrisly <laura.fabbiano.gomez@hotmail.com>
2023-06-02 20:59:41 +0200

    read the subjects - will do it later

---
+++ CPP/CPP00/ex02/Account.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/02 20:46:40 by brisly            #+#    #+#             */
+/*   Updated: 2023/06/02 20:59:17 by brisly           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int Account::getNbAccounts(void)
+{
+
+}
+
+static int Account::getTotalAmount(void)
+{
+
+}
+
+static int Account::getNbDeposits(void)
+{
+
+}
+
+static int	Account::getNbWithdrawals(void)
+{
+
+}
+
+static void	Account::displayAccountsInfos(void)
+{
+    
+}
