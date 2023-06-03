f4dd2af49d050ee6a8a41e221234fd71e02ddaa6
thebrisly <laura.fabbiano.gomez@hotmail.com>
2023-06-03 20:44:11 +0200

    ex01 done

--- CPP/CPP01/ex01/main.cpp
+++ CPP/CPP01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 21:36:10 by brisly            #+#    #+#             */
-/*   Updated: 2023/06/03 08:11:59 by brisly           ###   ########.fr       */
+/*   Updated: 2023/06/03 20:43:42 by brisly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,27 @@ Zombie* zombieHorde(int N, std::string name) //Continuer ça ici
 
 int main()
 {
+    int i = 0;
+    int n = 10;
+    std::cout << "Creating " << n << " zombies..." << std::endl;
+    Zombie *group;
+    group = zombieHorde(n, "Laura");
     
-}
+    std::cout << "\n-------------------------------\n" << std::endl;
+    Zombie *current = group;
+
+    std::cout << "Making them announce theirself" << std::endl;
+    while (i < n)
+    {
+        std::cout << i + 1 << " ";
+        current->announce();
+        i++;
+        current++;
+    }
+    i = 0;
+    
+    std::cout << "\n-------------------------------\n" << std::endl;
+
+    std::cout << "Deleting all the zombies in one line" << std::endl;
+    delete [] group;
+}  
