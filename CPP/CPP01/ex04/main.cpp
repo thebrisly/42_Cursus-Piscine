b57c6fa3d61422d48f6ae1a8820cb2e7512450be
thebrisly <laura.fabbiano.gomez@hotmail.com>
2023-06-06 11:59:02 +0200

    ex04 done

--- CPP/CPP01/ex04/main.cpp
+++ CPP/CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 21:50:11 by brisly            #+#    #+#             */
-/*   Updated: 2023/06/06 10:15:01 by brisly           ###   ########.fr       */
+/*   Updated: 2023/06/06 11:55:38 by brisly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,29 @@
 #include <fstream>
 #include <string>
 
-/*std::string replace_word(std::string to_search, std::string to_replace, std::string base)
+std::string replace_word(const std::string& to_search, const std::string& to_replace, const std::string& base) 
 {
-    std::string new_line;
-    int current = 0;
-    int previous = 0;
+    std::string final_string = base;
+    size_t found = 0;
+    size_t search_len = to_search.length();
+    size_t replace_len = to_replace.length();
 
-    while (temp[current])
+    while ((found = final_string.find(to_search, found)) != std::string::npos) //if s1 is found, found = position of the first occurence
     {
-        while (to_search[j])
-
+        final_string = final_string.substr(0, found) + to_replace + final_string.substr(found + search_len);
+        found += replace_len; //adter the word has been replaced, we start again by this position
     }
-    return (new_line);
-}*/
+
+    return final_string;
+}
 
 void if_in_of(std::ifstream &ifs, std::ofstream &ofs, std::string s1, std::string s2)
 {
     std::string     temp;
-    (void) s1; // a enlever
-    (void) s2; // a enlever
     
-    while (getline(ifs, temp))
+    while (getline(ifs, temp)) //takes the entire line of ifs and puts it in temp
     {
-        //temp = replace_word(s1, s2, temp);
+        temp = replace_word(s1, s2, temp);
         ofs << temp << std::endl;
     }
 }
@@ -54,14 +54,14 @@ int main(int argc, char **argv)
 
         infile.open(filename.c_str()); // a voir si a 42 je peux enlever ce c_str() -> chez moi je peux pas faire sans
         if (!infile) {
-            std::cout << "This file does not exist. Quitting the program... Try again" << std::endl;
+            std::cout << "\033[31mThis file does not exist. Try again\033[0m" << std::endl;
             return (1);
         }
 
         std::string     filename2 = filename + ".replace";
         outfile.open(filename2.c_str()); // a voir si a 42 je peux enlever ce c_str() -> chez moi je peux pas faire sans
         if (!outfile.is_open()) {
-            std::cout << "Failed to create the output file. Quitting the program... Try again" << std::endl;
+            std::cout << "\033[31mFailed to create the output file. Quitting the program... Try again\033[0m" << std::endl;
             return 1;
         }
         
