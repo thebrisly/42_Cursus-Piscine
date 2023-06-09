54f6e04a16f9b7cde099e00dbe5a69e83c5bcece
thebrisly <laura.fabbiano.gomez@hotmail.com>
2023-06-09 22:22:22 +0200

    CPP03 ex03 init

---
+++ CPP/CPP03/ex03/ClapTrap.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/08 21:27:52 by brisly            #+#    #+#             */
+/*   Updated: 2023/06/09 21:56:22 by brisly           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+# include <string>
+# include <iostream>
+
+class ClapTrap {
+    protected:
+        std::string     _name;
+        int             _hitPoints;
+        int             _energyPoints;
+        int             _attackDamage;
+
+    public:
+        ClapTrap(std::string name);                 // default constructor
+        ~ClapTrap();                                // default destructor
+        ClapTrap(ClapTrap const &src);              // copy constructor
+        ClapTrap& operator=(ClapTrap const &src);   // overloading operator for =
+
+        void attack(const std::string &target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+        void status(void);
+
+        std::string getName (void);
+        int getDamage (void);
+
+        
+};
+
+#endif
