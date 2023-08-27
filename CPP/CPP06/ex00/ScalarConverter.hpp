bb9dfedfffc4da11eb20731df8c60cee6189cb86
thebrisly <laura.fabbiano.gomez@hotmail.com>
2023-08-27 21:26:32 +0200

    CPP06 ex00 continued

--- CPP/CPP06/ex00/ScalarConverter.hpp
+++ CPP/CPP06/ex00/ScalarConverter.hpp
@@ -6,15 +6,49 @@
 /*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 19:32:49 by brisly            #+#    #+#             */
-/*   Updated: 2023/08/27 19:33:48 by brisly           ###   ########.fr       */
+/*   Updated: 2023/08/27 21:26:17 by brisly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
 
-static class ScalarConverter {
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <limits.h>
+#include <cfloat>
+#include <ostream>
+#include <stdlib.h>
 
+static class ScalarConverter 
+{
+
+    public :        
+        static void convert(std::string string);
+
+        class UnknowTypeException : public std::exception
+        {
+            public :
+
+                virtual const char* what() const throw()
+                {
+                    return ("The type you gave seems incorrect. Make sure that it's a double, float, char or int.");
+                }
+        };
+
+    private :
+        ScalarConverter();
+        ~ScalarConverter();
+        ScalarConverter(const ScalarConverter &src);
+        ScalarConverter &operator=(const ScalarConverter &src);
+
+        static int isInt(std::string string) const;
+        static int isFloat(std::string string) const;
+        static int isChar(std::string string) const;
+        static int isDouble(std::string string) const;
+        
+        
 };
 
 #endif
--- CPP/CPP06/ex00/main.cpp
+++ CPP/CPP06/ex00/main.cpp
@@ -6,13 +6,19 @@
 /*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 19:32:24 by brisly            #+#    #+#             */
-/*   Updated: 2023/08/27 19:33:58 by brisly           ###   ########.fr       */
+/*   Updated: 2023/08/27 21:26:15 by brisly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
-    
+    if (argc == 2)
+    {
+        std::string arg = argv[1];
+        ScalarConverter::convert(arg);
+    }
+    else
+        std::cerr << "2 args only needed : ./convert and a char, float, int or double"
 }
