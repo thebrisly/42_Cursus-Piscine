b296bd798ad65647266a1bfb72a3047d8291437e
thebrisly <laura.fabbiano.gomez@hotmail.com>
2023-08-27 21:42:48 +0200

    SC.cpp init

--- CPP/CPP06/ex00/ScalarConverter.cpp
+++ CPP/CPP06/ex00/ScalarConverter.cpp
@@ -6,8 +6,17 @@
 /*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 19:32:32 by brisly            #+#    #+#             */
-/*   Updated: 2023/08/27 19:33:51 by brisly           ###   ########.fr       */
+/*   Updated: 2023/08/27 21:41:26 by brisly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::~ScalarConverter() {}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &src) {}
+
+ScalarConverter &operator=(const ScalarConverter &src)
+{ return *this }
