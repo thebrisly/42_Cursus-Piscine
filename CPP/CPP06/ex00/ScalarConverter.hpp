35f9ce851448b3fe09063641ca31ce0c40215fa9
thebrisly <laura.fabbiano.gomez@hotmail.com>
2023-08-27 19:34:18 +0200

    CPP 06 init

---
+++ CPP/CPP06/ex00/ScalarConverter.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/27 19:32:49 by brisly            #+#    #+#             */
+/*   Updated: 2023/08/27 19:33:48 by brisly           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+static class ScalarConverter {
+
+};
+
+#endif
