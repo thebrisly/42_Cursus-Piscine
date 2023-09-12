7e9a329aaae2d2e3b9cf0025bb22ca996373584e
thebrisly <laura.fabbiano.gomez@hotmail.com>
2023-09-12 20:41:55 +0200

    main.cpp from the subject added

--- CPP/CPP07/ex00/main.cpp
+++ CPP/CPP07/ex00/main.cpp
@@ -6,13 +6,29 @@
 /*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 12:56:27 by brisly            #+#    #+#             */
-/*   Updated: 2023/09/11 12:57:00 by brisly           ###   ########.fr       */
+/*   Updated: 2023/09/12 20:41:37 by brisly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
-int main()
+int main( void ) 
 {
+	int a = 2;
+	int b = 3;
 	
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 }
