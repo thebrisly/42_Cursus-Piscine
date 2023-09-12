c0be2602e863b385abfef8c6f3f70a39e7a7f019
thebrisly <laura.fabbiano.gomez@hotmail.com>
2023-09-12 21:53:57 +0200

    CPP07 ex02 to be continued

--- CPP/CPP07/ex02/Array.hpp
+++ CPP/CPP07/ex02/Array.hpp
@@ -1,17 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Aray.hpp                                           :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 13:09:11 by brisly            #+#    #+#             */
-/*   Updated: 2023/09/11 13:09:26 by brisly           ###   ########.fr       */
+/*   Updated: 2023/09/12 21:53:28 by brisly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
+# include <iostream>
+
+template <typename T> class Array {
+	
+//construction with no parameter
+
+//constructor with n (creates array of n elements)
+
+// constructor by copy 
+
+//assignment operator
+
+//member size() that return the number of elements in the array
+
+//create an exception when accessing and wrong index
+
+	
+}
 
 
 #endif
--- CPP/CPP07/ex02/main.cpp
+++ CPP/CPP07/ex02/main.cpp
@@ -6,13 +6,17 @@
 /*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 13:09:01 by brisly            #+#    #+#             */
-/*   Updated: 2023/09/11 13:09:03 by brisly           ###   ########.fr       */
+/*   Updated: 2023/09/12 21:53:36 by brisly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "whatever.hpp"
+#include "array.hpp"
 
 int main()
 {
+	Array a;
 	
+    int arr[5] = { 1, 2, 3, 4, 5 };
+    Array<int> a(arr, 5);
+    a.print();
 }
