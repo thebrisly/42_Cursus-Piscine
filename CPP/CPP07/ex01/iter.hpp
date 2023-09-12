488bbcb16588c540eafed6be3d63385c22ed82a0
thebrisly <laura.fabbiano.gomez@hotmail.com>
2023-09-12 21:27:54 +0200

    CPP07 ex01 done

--- CPP/CPP07/ex01/iter.hpp
+++ CPP/CPP07/ex01/iter.hpp
@@ -6,12 +6,29 @@
 /*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 13:07:08 by brisly            #+#    #+#             */
-/*   Updated: 2023/09/11 13:07:27 by brisly           ###   ########.fr       */
+/*   Updated: 2023/09/12 21:24:51 by brisly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
+# include <iostream>
 
+template <typename T>
+void iter(T* array, size_t length, void (*function)(T&))
+{
+	size_t i = 0;
+	while (i < length)
+	{
+		function(array[i]);
+		i++;
+	}
+}
+
+template <typename T>
+void printElement(T& element)
+{
+    std::cout << element << " ";
+}
 
 #endif
--- CPP/CPP07/ex01/main.cpp
+++ CPP/CPP07/ex01/main.cpp
@@ -6,13 +6,29 @@
 /*   By: brisly <brisly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 13:06:56 by brisly            #+#    #+#             */
-/*   Updated: 2023/09/11 13:06:59 by brisly           ###   ########.fr       */
+/*   Updated: 2023/09/12 21:27:29 by brisly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "whatever.hpp"
+#include "iter.hpp"
+#include <iostream>
 
 int main()
 {
-	
+    std::cout << "intArray: ";
+	int intArray[] = {1, 2, 3, 4, 5};
+    iter(intArray, 5, printElement<int>);
+    std::cout << std::endl;
+
+    std::cout << "doubleArray: ";
+	double doubleArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    iter(doubleArray, 5, printElement<double>);
+    std::cout << std::endl;
+
+	std::cout << "stringArray: ";
+	std::string stringArray[] = {"hello", "my", "name", "is", "laura"};
+    iter(stringArray, 5, printElement<std::string>);
+    std::cout << std::endl;
+
+    return 0;
 }
