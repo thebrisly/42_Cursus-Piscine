901b49d143cb578752f2577cf416ff5f9ba91170
Lau <laura.fabbiano.gomez@hotmail.com>
2024-07-10 10:00:29 +0200

    training for exam6

---
+++ -- EXAMS/ExamRank6/exam6_text.c
@@ -0,0 +1,71 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <netinet/ip.h>
+
+int		count = 0, max_fd = 0;
+int		ids[65536];
+char	*msgs[65536];
+
+fd_set	rfds, wfds, afds;
+char	buf_read[1001], buf_write[42];
+
+
+// START COPY PASTE //
+
+int extract_message(char **buf, char **msg)
+{
+	char	*newbuf;
+	int	i;
+
+	*msg = 0;
+	if (*buf == 0)
+		return (0);
+	i = 0;
+	while ((*buf)[i])
+	{
+		if ((*buf)[i] == '\n')
+		{
+			newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
+			if (newbuf == 0)
+				return (-1);
+			strcpy(newbuf, *buf + i + 1);
+			*msg = *buf;
+			(*msg)[i + 1] = 0;
+			*buf = newbuf;
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+char *str_join(char *buf, char *add)
+{
+	char	*newbuf;
+	int		len;
+
+	if (buf == 0)
+		len = 0;
+	else
+		len = strlen(buf);
+	newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
+	if (newbuf == 0)
+		return (0);
+	newbuf[0] = 0;
+	if (buf != 0)
+		strcat(newbuf, buf);
+	free(buf);
+	strcat(newbuf, add);
+	return (newbuf);
+}
+
+// END COPY-PASTE
+
+
+void	fatal_error()
+{
+	write(2, "Fatal error\n", 12);
+	exit(1);
+}
